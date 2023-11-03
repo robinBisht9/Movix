b398f61b17039469d5bd6e112fb7dc5e2a5b62cd
robinBisht <robinbisht34@gmail.com>
2023-11-03 20:25:44 +0530

    Adding cast section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -4,6 +4,7 @@ import Carousel from "../../components/carousel/Carousel";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../components/switchTabs/switchTabs";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
 
 const Details = () => {
   const { mediatype, id } = useParams();
@@ -14,11 +15,10 @@ const Details = () => {
     `/${mediatype}/${id}/credits`
   );
 
-  console.log(videosData);
-  console.log(credits);
   return (
     <div>
       <DetailsBanner video={videosData?.results?.[0]} crews={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
     </div>
   );
 };
